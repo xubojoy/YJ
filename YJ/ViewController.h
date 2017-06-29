0e0c721ae53c38d4d4fa3e34e973df7ce40c9118
xubojoy <xubojoy1112@163.com>
2017-06-29 16:23:29 +0800

    Initial Commit

---
+++ YJ/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YJ
+//
+//  Created by xubojoy on 2017/6/29.
+//  Copyright © 2017年 xubojoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YJ/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  YJ
+//
+//  Created by xubojoy on 2017/6/29.
+//  Copyright © 2017年 xubojoy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
